397d3e78686fadd2955978de90dfbb4d02ae34a3
Dharsinipriya10 <51502811+Dharsinipriya10@users.noreply.github.com>
2019-06-08 14:16:26 +0530

    Update vowelcon.java

--- vowelcon.java
+++ vowelcon.java
@@ -4,10 +4,16 @@ public class vowelcon {
 	{
 		Scanner sc=new Scanner(System.in);
 		char ch = sc.next().charAt(0);
+		if(Character.isAlphabetic(ch))
 		if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'||ch=='A'||ch=='E'|| ch=='I'||ch=='O'|ch=='U')
+		{
             System.out.println(ch + " is vowel");
-        else
-            System.out.println(ch + " is consonant");
+		}
+        else{
+            System.out.println(ch + " is consonant");}
+		else
+		{	System.out.println("invalid");
+	}
  }
 }
 	
