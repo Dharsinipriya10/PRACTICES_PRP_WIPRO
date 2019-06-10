d453a7e43bd3eb7bcf3d617abcc9484e0e3cd0e7
Dharsinipriya10 <51502811+Dharsinipriya10@users.noreply.github.com>
2019-06-10 10:23:55 +0530

    Create powersum.java

---
+++ powersum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class powersum {
+public static void main(String []args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the number");
+	int n=sc.nextInt();
+	int k=sc.nextInt();
+System.out.println("the number:"+(int)Math.pow(n, k));
+}
+}
