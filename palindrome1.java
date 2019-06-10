35646ade546b96d8fd15cd0a830c3b33f87f5f29
Dharsinipriya10 <51502811+Dharsinipriya10@users.noreply.github.com>
2019-06-10 11:25:44 +0530

    Create palindrome1.java

---
+++ palindrome1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class palindrome1 {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the num");
+		int num= sc.nextInt();
+		int temp= num;
+		int sum=0,r;
+		while(num>0)
+		{
+			r=num%10;
+			sum=(sum*10)+r;
+			num=num/10;
+		}
+		if(temp==sum)
+		{
+			System.out.println("yes");
+		}
+		else
+			
+		{
+			System.out.println("no");
+		}
+	}
+
+}
