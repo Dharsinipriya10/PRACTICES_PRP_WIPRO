917e6cac20ed24e4f5c828a2b15b2e2c04acd72d
Dharsinipriya10 <51502811+Dharsinipriya10@users.noreply.github.com>
2019-06-11 15:57:33 +0530

    Create weekends.java

---
+++ weekends.java
@@ -0,0 +1,36 @@
+public class weekends {
+	public static void main(String[]args)
+	{
+		Scanner scan=new Scanner(System.in);
+		String days=scan.next();
+		switch(days)
+		{
+		 case "sunday":
+             System.out.println("yes");
+             break;
+         case "monday":
+             System.out.println("no");
+             break;
+         case "tuesday":
+             System.out.println("no");
+             break;
+         case "wednesday":
+             System.out.println("no");
+             break;
+         case "thursday":
+             System.out.println("no");
+             break;
+         case "friday":
+             System.out.println("no");
+             break;
+         case "saturday":
+             System.out.println("no");
+             break;
+         default:
+             System.out.println("invalid");
+             break;
+     }}}
+
+	
+
+
