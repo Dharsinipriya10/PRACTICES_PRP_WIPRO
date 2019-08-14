0b111cbb82ea59b129a652d21abde366512a8c21
Dharsinipriya10 <51502811+Dharsinipriya10@users.noreply.github.com>
2019-08-14 14:41:49 +0530

    Create Pangram.java

---
+++ Pangram.java
@@ -0,0 +1,19 @@
+public class Pangram {
+public static void main(String [] args){
+	Scanner scan = new Scanner(System.in);
+	String s= scan.nextLine();
+	s=s.toLowerCase();
+	String alpha="abcdefghijklmnopqrstuvwxyz";
+	int count=0;
+	boolean flag=false;
+	for(int i=0;i<alpha.length();i++){
+	   for(int j=0;j<s.length();j++){
+		  if(alpha.charAt(i)==s.charAt(j)){
+				count++;
+				break;
+				
+			}
+		}
+	
+	}
+	
