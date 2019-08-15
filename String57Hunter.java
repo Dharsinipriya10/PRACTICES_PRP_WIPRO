6705b07e0a32fbf729eccc1e0d7bce5be1a952bd
Dharsinipriya10 <51502811+Dharsinipriya10@users.noreply.github.com>
2019-08-15 14:54:49 +0530

    Create String57Hunter.java

---
+++ String57Hunter.java
@@ -0,0 +1,32 @@
+public class String57Hunter {
+public static void main(String [] args){
+	Scanner scan = new Scanner (System.in);
+	String str= scan.nextLine();
+	String str1= scan.nextLine();
+	String str2=" ";
+	String st=" ";
+	boolean flag = false;
+	a:
+	for(int i=0;i<str.length()-2;i++){
+		for(int j=0;j<str1.length()-2;j++){
+			st=str.substring(i,i+2);
+			str2=str1.substring(j,j+2);
+			if(st.equals(str2))
+			{ 
+				flag= true;
+				break a;
+			}
+		}
+	}
+	if(flag== true){
+				System.out.println("yes");
+	}
+			else
+			{
+				System.out.println("no");
+			}
+			
+		
+}
+
+}
